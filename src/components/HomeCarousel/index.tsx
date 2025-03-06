6be71c92686fece264b4f8c5c85cb06025ac6ad5
jayanta dey <jayantadey398@gmail.com>
2025-03-06 11:53:33 +0530

    Update index.tsx

--- src/components/HomeCarousel/index.tsx
+++ src/components/HomeCarousel/index.tsx
@@ -57,7 +57,7 @@ const HomeCarousel = () => {
       ssr={true}
       infinite={true}
       // autoPlay={deviceType !== "mobile"}
-      // autoPlay={true}
+      autoPlay={true}
       autoPlaySpeed={3000}
       keyBoardControl={true}
       customTransition="all .5s"
